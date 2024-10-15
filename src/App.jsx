9dbf6113ec36deab046c18e13e559d434fd2c36b
Ghazale <ghazale.mollamohammad@gmail.com>
2024-10-15 12:16:49 +0330

    Implemented Routes

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./UI/Home";
+import Cart from "./features/cart/Cart";
+import Menu from "./features/menu/Menu";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "cart",
+    element: <Cart />,
+  },
+  {
+    path: "menu",
+    element: <Menu />,
+  },
+
+  { path: "order/new", element: <CreateOrder /> },
+  {
+    path: "order/:orderId",
+    element: <Order />,
+  },
+]);
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
