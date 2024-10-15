2ae529c74ecb6e5be0a3239d3c4774e5b86d6b09
Ghazale <ghazale.mollamohammad@gmail.com>
2024-10-15 13:35:08 +0330

    Fetched Orders from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./UI/Home";
+import Home from "./features/UI/Home";
 import Cart from "./features/cart/Cart";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./features/UI/AppLayout";
 import Error from "./features/UI/Error";
 
@@ -30,6 +30,8 @@ const router = createBrowserRouter([
       {
         path: "order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
--- src/features/UI/Header.jsx
+++ src/features/UI/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchQuery from "../order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchQuery/>
       <p>username</p>
     </header>
   );
